a1f2aff3ff21d78f2ebc366a50c93faf8e5ead0e
sourabh <sourabhprajapati759@gmail.com>
2026-02-25 00:58:16 +0530

    React setup with Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+   return <div className=" min-h-screen overflow-x-hidden bg-gradient-to-br from-black via-zinc-900 to-slate-800 text-white font-bold">
+      <div>Hello World</div></div>
+}
+
+
+export default App;
